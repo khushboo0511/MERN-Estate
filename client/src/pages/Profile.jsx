14b189d0a0a3b4777db286735de7dfdb71836b90
Khushboo0511 <khushboochaudhary0511@gmail.com>
2024-04-03 11:07:28 +0530

    complete profile paded UI

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,9 +1,59 @@
-import React from 'react'
+import React from 'react';
+import { useSelector } from 'react-redux'; // Assuming you're using Redux for state management
 
 const Profile = () => {
-  return (
-    <div>Profile</div>
-  )
-}
+  const { currentUser } = useSelector((state) => state.user); // Assuming currentUser is stored in Redux state
 
-export default Profile
+  return (
+    <div className='p-5 max-w-lg mx-auto'>
+      <div className='text-center'>
+        <h1 className='text-3xl font-semibold mb-5'>Profile</h1>
+        <div className='mb-5'>
+          <img src={currentUser.avatar} alt='profile' className='rounded-full h-24 w-24 object-cover cursor-pointer mx-auto' />
+        </div>
+      </div>
+      <form>
+        <div className='mb-5'>
+          <input
+            id='username'
+            type='text'
+            placeholder='Enter your username'
+            defaultValue={currentUser.username}
+            className='border p-3 rounded-lg w-full focus:outline-none focus:ring focus:border-blue-300'
+          />
+        </div>
+        <div className='mb-5'>
+          <input
+            id='email'
+            type='email'
+            placeholder='Enter your email'
+            defaultValue={currentUser.email}
+            className='border p-3 rounded-lg w-full focus:outline-none focus:ring focus:border-blue-300'
+            readOnly // email is non editable
+          />
+        </div>
+        <div className='mb-5'>
+          <input
+            id='username'
+            type='text'
+            placeholder='Enter new password'
+            defaultValue={currentUser.password}
+            className='border p-3 rounded-lg w-full focus:outline-none focus:ring focus:border-blue-300'
+          />
+        </div>
+        <div className='text-center'>
+          <button type='submit' className='bg-slate-700 text-white p-3 rounded-lg shadow w-full uppercase hover:opacity-85 disabled:opacity-60'>
+            Update
+          </button>
+        </div>
+      </form>
+      
+      <div className='flex justify-between mt-5 font-semibold'>
+        <span className='text-red-700 cursor-pointer'>Delete Account</span>
+        <span className='text-red-700 cursor-pointer'>Sign Out</span>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -56,7 +56,7 @@ exports.google = async (req, res, next) => {
             Math.random().toString(36).slice(-4),
           email: req.body.email,
           password: hashedPassword,
-          avatar: req.body.photo,
+          avatar: req.body.photoUrl,
         });
         await newUser.save();
         const token = jwt.sign({ id: newUser._id }, 'ascnnvbivbiwegb5654946gerge44');
