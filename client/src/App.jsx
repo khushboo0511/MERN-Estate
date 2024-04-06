ec783a0eb677808503a8449b0d5ac6d4df881146
Khushboo0511 <khushboochaudhary0511@gmail.com>
2024-04-06 10:02:23 +0530

    completed hone and about page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -18,7 +18,8 @@ function App() {
   <BrowserRouter>
   <Header />
   <Routes>
-    <Route path="/" element={<Home />} />
+  <Route path="/" element={<Home />} />
+    <Route path="/home" element={<Home />} />
     <Route path="/about" element={<About />} />
     <Route path='/listing/:listingId' element={<Listing />} />
     <Route path='/search' element={<Search />} />
